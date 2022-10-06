da4c241d793d542f3ad343a3423fdc5d64a49377
Sagimbaev <sagimbaevilyas05@gmail.com>
2022-10-06 15:38:37 +0600

    .

---
+++ app.js
@@ -0,0 +1,10 @@
+const arr = ['зеленый', 'желтый', 'красный'];
+for (var i = 0; i< arr.length; i++) {
+   if (arr[i] === 'зеленый') {
+      console.log('идти');
+   } else if (arr[i]=== 'желтый') {
+      console.log('жди');
+   } else if (arr[i]=== 'красный') {
+      console.log('стой');
+  }
+}
